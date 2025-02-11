1ad4bfdc74c9aa264e9f85c99d00ea27e6a8d7c3
Mahmood Haithami <mss3331@hotmail.com>
2025-02-11 20:46:59 +0800

    Added PHP

---
+++ XmlHttpReq.js
@@ -0,0 +1,30 @@
+
+document.addEventListener('DOMContentLoaded',run);
+
+function run(){
+    document.querySelector('button').addEventListener('click', deleteBody);
+}
+
+function deleteBody(event){
+    document.querySelector('body').remove();
+    setTimeout(getNewBody,1000);
+}
+
+function getNewBody(){
+    // const xhr = new XMLHttpRequest();
+    $("#scholar").load("req-page.php");
+    // xhr.open('GET', 'https://scholar.google.com/citations?user=Aj2-v14AAAAJ&hl=en');
+
+    // xhr.onreadystatechange=function() {
+    //     if(this.readyState===4 && this.status ===200)
+    //     {   
+    //         const new_page = this.responseText;
+    //         const page = new DOMParser().parseFromString(new_page,'text/html');
+    //         document.querySelector('html').appendChild(page.body);
+    //         console.log(document.body);
+    //     }
+            
+        
+    // };
+    // xhr.send();
+}
---
+++ req-page.php
@@ -0,0 +1,2 @@
+
+echo file_get_contents("https://scholar.google.com/citations?user=Aj2-v14AAAAJ&hl=en");
